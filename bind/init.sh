1bc4068c01636ae39b2fd39f3bef184f538bdc70
vdo <vdo@greyfaze.net>
2019-11-06 11:32:46 +0100

    Keep old config on startup and increase verbosity

--- bind/init.sh
+++ bind/init.sh
@@ -5,7 +5,7 @@ export UPDATE_HOST=${UPDATE_HOST:-127.0.0.1}
 export RECORD_TTL=${RECORD_TTL:-30}
 export DLR='$'
 
-envsubst < "/etc/bind/templates/template.zone" > "/etc/bind/$ZONE.zone"
-envsubst < "/etc/bind/templates/named.conf" > "/etc/bind/named.conf"
+[ -f "/etc/bind/$ZONE.zone" ] || envsubst < "/etc/bind/templates/template.zone" > "/etc/bind/$ZONE.zone"
+[ -f "/etc/bind/named.conf" ] || envsubst < "/etc/bind/templates/named.conf" > "/etc/bind/named.conf"
 
-/usr/sbin/named -d 1 -f -c /etc/bind/named.conf
+/usr/sbin/named -d 2 -f -c /etc/bind/named.conf
